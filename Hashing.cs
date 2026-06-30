2b3893bb82540e64aa012c3dd7672c3930f14020
Matlauk <mjlaukala@gmail.com>
2026-06-29 20:42:56 -0700

    init

---
+++ Hashing.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Numerics;
+
+public static class Hashing
+{
+	private const uint PRIME_X = 0x27d4eb2d;
+	private const uint PRIME_Y = 0x85ebca6b;
+	private const uint PRIME_Z = 0xc2b2ae35;
+	
+	
+	private static uint StringHash(string value)
+	{
+		unchecked
+		{
+			uint hash = 2166136261;
+			for (int i = 0; i < value.Length; i++)
+			{
+				hash ^= value[i];
+				hash *= 16777619;
+			}
+
+			return hash;
+		}
+	}
+
+	// --------------------------------------------------
+	// Final avalanche mixer
+	// --------------------------------------------------
+
+	private static uint Avalanche(uint h)
+	{
+		unchecked
+		{
+			h ^= h >> 16;
+			h *= 0x7feb352d;
+			h ^= h >> 15;
+			h *= 0x846ca68b;
+			h ^= h >> 16;
+
+			return h;
+		}
+	}
+
+	public static uint Rehash(uint value)
+	{
+		return Avalanche(value);
+	}
+	
+	// --------------------------------------------------
+	// 1D Hash
+	// --------------------------------------------------
+
+	public static uint Hash1(int x, int seed)
+	{
+		unchecked
+		{
+			uint h = (uint)seed;
+
+			h ^= (uint)x * PRIME_X;
+			h = BitOperations.RotateLeft(h, 15);
+
+			return Avalanche(h);
+		}
+	}
+	
+	public static uint Hash1(float x, int seed)
+	{
+		unchecked
+		{
+			uint hx = BitConverter.SingleToUInt32Bits(x);
+
+			uint h = (uint)seed;
+
+			h ^= hx * PRIME_X;
+			h = BitOperations.RotateLeft(h, 15);
+
+			return Avalanche(h);
+		}
+	}
+
+	// --------------------------------------------------
+	// 2D Hash
+	// --------------------------------------------------
+
+	public static uint Hash2(int x, int z, int seed)
+	{
+		unchecked
+		{
+			uint h = (uint)seed;
+
+			h ^= (uint)x * PRIME_X;
+			h = BitOperations.RotateLeft(h, 15);
+
+			h ^= (uint)z * PRIME_Y;
+			h = BitOperations.RotateLeft(h, 13);
+
+			return Avalanche(h);
+		}
+	}
+	
+	public static uint Hash2(float x, float z, int seed)
+	{
+		unchecked
+		{
+			uint hx = BitConverter.SingleToUInt32Bits(x);
+			uint hz = BitConverter.SingleToUInt32Bits(z);
+
+			uint h = (uint)seed;
+
+			h ^= hx * PRIME_X;
+			h = BitOperations.RotateLeft(h, 15);
+
+			h ^= hz * PRIME_Y;
+			h = BitOperations.RotateLeft(h, 13);
+
+			return Avalanche(h);
+		}
+	}
+
+	// --------------------------------------------------
+	// 3D Hash
+	// --------------------------------------------------
+
+	public static uint Hash3(int x, int y, int z, int seed)
+	{
+		unchecked
+		{
+			uint h = (uint)seed;
+
+			h ^= (uint)x * PRIME_X;
+			h = BitOperations.RotateLeft(h, 15);
+
+			h ^= (uint)y * PRIME_Y;
+			h = BitOperations.RotateLeft(h, 13);
+
+			h ^= (uint)z * PRIME_Z;
+			h = BitOperations.RotateLeft(h, 17);
+
+			return Avalanche(h);
+		}
+	}
+	
+	public static uint Hash3(float x, float y, float z, int seed)
+	{
+		unchecked
+		{
+			uint hx = BitConverter.SingleToUInt32Bits(x);
+			uint hy = BitConverter.SingleToUInt32Bits(y);
+			uint hz = BitConverter.SingleToUInt32Bits(z);
+
+			uint h = (uint)seed;
+
+			h ^= hx * PRIME_X;
+			h = BitOperations.RotateLeft(h, 15);
+
+			h ^= hy * PRIME_Y;
+			h = BitOperations.RotateLeft(h, 13);
+
+			h ^= hz * PRIME_Z;
+			h = BitOperations.RotateLeft(h, 17);
+
+			return Avalanche(h);
+		}
+	}
+
+	// --------------------------------------------------
+	// Hash -> [0, 1]
+	// --------------------------------------------------
+
+	public static float HashToFloat01(uint h)
+	{
+		return h / (float)uint.MaxValue;
+	}
+
+	// --------------------------------------------------
+	// Hash -> [-1, 1]
+	// --------------------------------------------------
+
+	public static float HashToFloatSigned(uint h)
+	{
+		return (h / (float)uint.MaxValue) * 2f - 1f;
+	}
+
+	// --------------------------------------------------
+	// Hash -> Range
+	// --------------------------------------------------
+
+	public static float HashToFloat(uint h, float min, float max)
+	{
+		return min + (max - min) * HashToFloat01(h);
+	}
+}
